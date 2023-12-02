24286e33b1958b8339ab0edd00bab9b5994e27b2
hyeran0513 <hr_0513@naver.com>
2023-12-02 14:50:18 +0900

    데이터 바인딩

--- vue3_project/src/App.vue
+++ vue3_project/src/App.vue
@@ -1,39 +1,17 @@
 <template>
   <div>
-    <button 
-      type="button" 
-      class="btn btn-primary" 
-      v-on:click="updateStringSample">
-      {{ stringSample }}
-    </button>
-
-    <button 
-      type="button" 
-      class="btn btn-secondary" 
-      v-on:click="updateArraySample">
-      {{ arraySample }}
-    </button>
+    <div>{{ name }}</div>
+    <input type="text" :value="name" />
+    <button type="button" @click="hello">click</button>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import {ref} from 'vue';
+// 데이터 바인딩 v-bind: -> : / v-on:click -> @click 으로 사용 가능
+const name = ref("hrkim");
 
-// 문자열, 배열, 객체 등 모두 사용 가능하나 사용 시 value를 붙여야 함
-const stringSample = ref('hrkim');
-
-// 배열이나 객체일 경우 reactive 사용
-const arraySample = reactive({ id: 'hrkim' });
-
-const updateStringSample = () => {
-  stringSample.value = 'hrkim2';
-} 
-
-const updateArraySample = () => {
-  arraySample.id = 'hrkim3';
+const hello = () => {
+  name.value = "coding";
 }
 </script>
-
-<style scoped>
-
-</style>
